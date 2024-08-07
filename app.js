1c35bc105b6a2950ae6eec9264564f8c808ec838
jenilindafofa <126267019+jenilindafofa@users.noreply.github.com>
2024-08-07 09:10:43 -0300

    g

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 alert('boas vindas ao jogo do numero secreto')
 let numeroSecreto=29
 prompt ('escolha um numero de 1 a 30')
+alert('Boas vindas ao numero secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
 if(chute == numeroSecreto) {
     console.log('Isso aí! Você descobriu o número secreto (29)');
 
