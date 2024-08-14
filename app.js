6f0a2fa149c71da42dabaf60d29cb7f9c218d68f
jenilindafofa <126267019+jenilindafofa@users.noreply.github.com>
2024-08-14 08:55:42 -0300

    11

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-
-
 alert('Boas vindas ao numero secreto');
-    let numeroSecreto = 6;
-    console.log(numeroSecreto)
-    let chute
-    //enquanto
-    chute não for igual ao numero Secreto
-    while
-    (chute != numeroSecreto) {
-      chute = prompt('Escolha um número entre 1 e 10');
-      // se chute for igual ao número secreto
-      if (chute == numeroSecreto) {
-          alert(`Isso aí! Você descobriu o número secreto
-    ${numeroSecreto}`);
-      } else {
-          if (chute > numeroSecreto) {
-          alert (`O numeroSecreto é menor que ${chute}`);
-          } else {
-          alert (`O numeroSecreto é maior que ${chute}`);
-          }
-      }
-    }
+let numeroSecreto = 10;
+console.log(numeroSecreto)
+let chute
+//enquanto chute não for igual ao numero Secreto
+while (chute!= numerosecreto){
+chute = prompt('Escolha um numero entre 1 e 20');
+//se chute for igual ao número secreto
+if(chute == numerosecreto){
+alert('Isso aí! Você descobriu o numero secreto ${numeroSecreto}');
+}else{
+if(chute > numeroSecreto){
+alert('O numeroSecreto é menor que ${chute}');
+}else{
+alert('O numeroSecreto é maior que ${chute}');
+}
+}
+}
